b3016afddecc7fa69c7866d8e357159dbb7ef16b
GyeonHee <hyungi4833@gmail.com>
2025-07-31 14:11:54 +0900

    엔진/게임 분리 초기세팅 완료

---
+++ Engine/Actor/Actor.cpp
@@ -0,0 +1,140 @@
+#include "Actor.h"
+#include "Utils/Utils.h"
+#include "Engine.h"
+#include "Level/Level.h"
+
+#include <Windows.h>
+#include <iostream>
+
+Actor::Actor(const char* image, Color color, const Vector2& position)
+	: color(color), position(position)
+{
+	// 문자열 길이
+	width = (int)strlen(image);
+
+	// 메모리 할당
+	this->image = new char[width + 1];
+
+	// 문자열 복사
+	strcpy_s(this->image, width + 1, image);
+}
+Actor::~Actor()
+{
+	// 메모리 해제
+	SafeDeleteArray(image);
+}
+
+// 이벤트 함수
+
+// 객체 생애주기(Lifetime)에 1번만 호출됨 (초기화 목적)
+void Actor::BeginPlay()
+{
+	hasBeganPlay = true;
+}
+
+// 프레임 마다 호출 (반복성 작업 / 지속성이 필요한 작업)
+void Actor::Tick(float deltaTime)
+{
+
+}
+
+// 그리기 함수
+void Actor::Render()
+{
+	// Win32 API
+	// 커서 위치 이동
+
+	// 콘솔 출력을 제어하는 핸들 얻어오기
+	//static HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// 커서 이동
+	Utils::SetCursorPosition(position);
+
+	// 색상 설정
+	Utils::SetConsoleTextColor(color);
+
+	//그리기
+	std::cout << image;
+}
+
+void Actor::SetPosition(const Vector2& newPosition)
+{
+	// 콘솔 출력을 제어하는 핸들 얻어오기
+	//static HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// 예외처리 (화면 벗어났는지 확인)
+
+	// 왼쪽 가장자리가 화면 왼쪽을 벗어났는지
+	if (newPosition.x < 0)
+	{
+		return;
+	}
+
+	// 오른쪽 가장자리가 화면 오른쪽을 벗어났는지
+	if (newPosition.x + width - 1 > Engine::Get().Width())
+	{
+		return;
+	}
+
+	// 위쪽 가장자리가 화면의 위를 벗어났는지
+	if (newPosition.y < 0)
+	{
+		return;
+	}
+
+	// 화면 아래를 벗어났는지
+	if (newPosition.y - 1 > Engine::Get().Height())
+	{
+		return;
+	}
+
+	// 같으면 업데이트 안함
+	if (position == newPosition)
+	{
+		return;
+	}
+	// 지울 위치 확인
+	Vector2 direction = newPosition - position;
+	position.x = direction.x >= 0 ? position.x : position.x + width - 1;
+
+	// 커서 이동
+	Utils::SetCursorPosition(position);
+
+	// 문자열 길이 고려해서 지울 위치 확인해야 함
+	std::cout << ' ';
+
+	position = newPosition;
+}
+
+Vector2 Actor::Position() const
+{
+	return position;
+}
+
+void Actor::SetSortingOrder(unsigned int sortingOrder)
+{
+	this->sortingOrder = sortingOrder;
+}
+
+void Actor::SetOwner(Level* newOwner)
+{
+	owner = newOwner;
+}
+
+Level* Actor::GetOwner()
+{
+	return owner;
+}
+
+void Actor::Destroy()
+{
+	// 삭제 요청 되었다고 설정
+	isExpired = true;
+
+	owner->DestroyActor(this);
+}
+
+void Actor::QuitGame()
+{
+	Engine::Get().Quit();
+}
---
+++ Engine/Level/Level.cpp
@@ -0,0 +1,193 @@
+#include "Level.h"
+#include "Actor/Actor.h"
+#include "Utils/Utils.h"
+#include <iostream>
+
+Level::Level()
+{
+
+}
+Level::~Level()
+{
+	// null 확인 후 액터 제거
+	for (Actor* actor : actors)
+	{
+		//// null 확인 후 Actor 제거
+		//if (actor)
+		//{
+		//	// 삭제 및 메모리 정리
+		//	delete actor;
+		//	actor = nullptr;
+		//}
+
+		SafeDelete(actor);
+	}
+}
+
+// 레벨에 액터를 추가할 때 사용
+void Level::AddActor(Actor* newActor)
+{
+	// 예외처리(중복 여부 확인) 필요
+
+	// push_back / emplace_back : 배열 맨 뒤에 새로운 항목 추가하는 함수
+
+	// 대기 배열에 추가
+	addRequestedActors.emplace_back(newActor);
+
+	//newActor->SetOwner(this);
+}
+
+void Level::DestroyActor(Actor* destroyedActor)
+{
+	// 대기 배열에 추가
+	destroyRequestedActors.emplace_back(destroyedActor);
+}
+
+// 이벤트 함수
+
+// 객체 생애주기(Lifetime)에 1번만 호출됨 (초기화 목적)
+void Level::BeginPlay()
+{
+	for (Actor* const actor : actors)
+	{
+		// 액터 처리 여부 확인
+		if (!actor->isActive || actor->isExpired)
+		{
+			continue;
+		}
+
+		// 이미 호출된 개체는 건너뛰기
+		if (actor->HasBeganPlay())
+		{
+			continue;
+		}
+
+		actor->BeginPlay();
+	}
+}
+
+// 프레임 마다 호출 (반복성 작업 / 지속성이 필요한 작업)
+void Level::Tick(float deltaTime)
+{
+	for (Actor* const actor : actors)
+	{
+		// 액터 처리 여부 확인
+		if (!actor->isActive || actor->isExpired)
+		{
+			continue;
+		}
+
+		actor->Tick(deltaTime);
+	}
+}
+
+// 그리기 함수
+void Level::Render()
+{
+	// 그리기 전에 정렬 순서 기준으로 재배치(정렬)
+	SortActorsBySortingOrder();
+
+	// Render Pass
+	for (Actor* const actor : actors)
+	{
+		// 액터 처리 여부 확인
+		if (!actor->isActive || actor->isExpired)
+		{
+			continue;
+		}
+
+		// 검사(같은 위치에 정렬 순서 높은 액터가 있는지 확인)
+		Actor* searchedActor = nullptr;
+		for (Actor* const otherActor : actors)
+		{
+			// 같은 액터면 무시
+			if (actor == otherActor)
+			{
+				continue;
+			}
+
+			// 위치가 같은 액터 확인
+			if (actor->Position() == otherActor->Position())
+			{
+				// 정렬 순서 비교 후 액터 저장
+				if (actor->sortingOrder < otherActor->sortingOrder)
+				{
+					// 저장 및 루프 종료
+					searchedActor = otherActor;
+					break;
+				}
+			}
+		}
+
+		// 어떤 액터와 같은 위치에 정렬 순서가 더 높은 액터가 있으면
+		// 그리지 않고 건너뛰기
+		if (searchedActor)
+		{
+			continue;
+		}
+
+		// 드로우 콜
+		actor->Render();
+	}
+}
+
+void Level::ProcessAddAndDestroyActors()
+{
+	// actors 배열에서 제외 처리
+	for (auto iterator = actors.begin(); iterator != actors.end();)
+	{
+		// 삭제 요청된 액터인지 확인 후 배열에서 제외
+		if ((*iterator)->isExpired)
+		{
+			// erase 함수를 사용하면 iterator가 무효화되기 때문에
+			// 반환되는 값을 저장해야함/
+			iterator = actors.erase(iterator);
+			continue;
+		}
+
+		++iterator;
+	}
+
+	// destroyedActors 배열을 순회하면서 액터 delete
+	for (auto* actor : destroyRequestedActors)
+	{
+		// 액터가 그렸던 곳 지우기
+		Utils::SetCursorPosition(actor->position);
+
+		// 콘솔에 빈문자 출력해서 지우기
+		for (int i = 0; i < actor->width; ++i)
+		{
+			std::cout << " ";
+		}
+
+		// 리소스 해제
+		SafeDelete(actor);
+	}
+
+	// 배열 초기화
+	destroyRequestedActors.clear();
+
+	// addRequestedActors 배열을 순회하면서 새로운 액터 add
+	for (auto* const actor : addRequestedActors)
+	{
+		actors.emplace_back(actor);
+		actor->SetOwner(this);
+	}
+
+	addRequestedActors.clear();
+}
+
+void Level::SortActorsBySortingOrder()
+{
+	for (int i = 0; i < (int)actors.size(); ++i)
+	{
+		for (int j = 0; j < (int)actors.size() - 1; ++j)
+		{
+			// sortingOrder 값이 클수록 뒤 쪽에 배치
+			if (actors[j]->sortingOrder > actors[j + 1]->sortingOrder)
+			{
+				std::swap(actors[j], actors[j + 1]);
+			}
+		}
+	}
+}
